f8f6e3e40be835607b4af755372ae2d21d6b8a02
Cheng Tang <cheng21tang@yahoo.com>
2021-03-26 01:19:06 -0700

    adding functions for prompts for password length and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -12,10 +12,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-  
+
 }
 
-function passLength() {
-  var passLength = prompt("How long do you want your password to be?")
+
+
+
+// logs user's selection of their password length
+function log(userPassLength) {
+  console.log(userPassLength);
 }
+
+// prompts the user to enter a password length from 8-128 characters
+function passLength() {
+  var userPassLength = prompt("Please choose a password length between 8 - 128 characters long");
+  log(userPassLength);
+    if (userPassLength < 8) {
+      prompt("You must choose a password length of 8 - 128 characters long");
+    } else {
+      alert("thank you")
+    }
+  }
 passLength();
+
+
+// logs user's selection of their special characters
+function log(userSpecialChar) {
+  console.log(userSpecialChar);
+}
+
+// prompts the user to select one or all the users of LOWERCASE, UPPERCASE, NUMERICS, and/or SPECIAL CHARACTERS
+function specChar() {
+  var userSpecialChar = prompt("You must to include in your password either LOWERCASE, UPPERCASE, NUMERICS, and/or SPECIAL CHARACTERS");
+  log(userSpecialChar);
+}
+specChar();
