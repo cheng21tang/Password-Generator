f7d1d146358f73e35e5573e36f320dc3b9b56504
Cheng Tang <cheng21tang@yahoo.com>
2021-03-26 15:31:44 -0700

    adding pseudo code to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,48 @@
+// <--------Declaring variables-------->
+// Create a Prompt box to store in a var called "passwordLength" 
+// Create a var "lowercaseArray" to store all lowercase characters (example: ["a", "b"....])
+// Create a var "uppoercaseArray" to store all uppercase characters
+// Create a var "numbersArray" to store all number characters
+// Create a var "symbolsArray" to store all special(symbols) characters
+// Create a var "password" to store my password (make this a empty string to start)
+// Create a var "passwordArray" to store possible characters (make this an empty array to start)
+// Create a var "hasLowercase" to store users input
+// Create a var "hasUppercase" to store users input
+// Create a var "hasNumberscase" to store users input
+// Create a var "hasSpecialcase" to store users input
+// Create a var "random" to store users input
+
+// function generatePassword() {
+// <--------Declaring variables-------->
+// Check if "passwordLength" is between 8-128
+// If it not a valid number, alert the user it is not a valid number
+// Else provide the next prompt
+// Create a Confirm box with a question (click OK to confirm lowercase characters) and store it in a var "hasLowercase" (it will contain true)
+// Create a Confirm box with a question (click OK to confirm uppercase characters) and store it in a var "hasUppercase" (it will contain true)
+// Create a Confirm box with a question (click OK to confirm numbers characters) and store it in a var "hasNumberscase" (it will contain true)
+// Create a Confirm box with a question (click OK to confirm special characters) and store it in a var "hasSpecialcase" (it will contain true)
+// If all Confirm box questions are "false" then I create Alert box to alert the user ("You need to choose 1 of the whatever") (if all false go back to line 16 and start again or end (error in creating password))
+// If user selected lowercase characters, which means "hasLowercase" is "true" then concat "passwordArray" with "lowercaseArray"
+// and choose some random character from "lowercaseArray" and concat with "password" (hint: use Math.floor and Math.Random) (it will be lowercaseArray[random])
+// If user selected uppercase characters, which means "hasUppercase" is "true" then concat "passwordArray" with "uppercaseArray"
+// and choose some random character from "uppercaseArray" and concat with "password"
+// If user selected numberscase characters, which means "hasNumberscase" is "true" then concat "passwordArray" with "numbersArray"
+// and choose some random character from "numberscaseArray" and concat with "password"
+// If user selected specialcase characters, which means "hasSpecialCase" is "true" then concat "passwordArray" with "specialcaseArray"
+// and choose some random character from "specialcaseArray" and concat with "password"
+// Iterate starting from length of the "password" var to "passwordLength" (using for loop)
+// Concat "password" with random characters from "passwordArray"
+// return "password"
+// }
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
