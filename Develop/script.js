133be3ff562bc85be95f9e8c57a5c3fbef8532f4
Cheng Tang <cheng21tang@yahoo.com>
2021-03-26 18:35:25 -0700

    added var for hasLowercase, hasUppercase, hasNumbers, hasSpecials, random

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,15 @@
 // function generatePassword() {
-// Create a Prompt box to store in a var called "passwordLength" 
-// Create a var "lowercaseArray" to store all lowercase characters (example: ["a", "b"....])
-// Create a var "uppoercaseArray" to store all uppercase characters
-// Create a var "numbersArray" to store all number characters
-// Create a var "specialArray" to store all special(symbols) characters
-// Create a var "password" to store my password (make this a empty string to start)
-// Create a var "passwordArray" to store possible characters (make this an empty array to start)
+    // Create a Prompt box to store in a var called "passwordLength" 
+    // Create a var "lowercaseArray" to store all lowercase characters (example: ["a", "b"....])
+    // Create a var "uppoercaseArray" to store all uppercase characters
+    // Create a var "numbersArray" to store all number characters
+    // Create a var "specialArray" to store all special(symbols) characters
+    // Create a var "password" to store my password (make this a empty string to start)
+    // Create a var "passwordArray" to store possible characters (make this an empty array to start)
 // Create a var "hasLowercase" to store users input
 // Create a var "hasUppercase" to store users input
-// Create a var "hasNumberscase" to store users input
-// Create a var "hasSpecialcase" to store users input
+// Create a var "hasNumbers" to store users input
+// Create a var "hasSpecials" to store users input
 // Create a var "random" to store users input
 // Check if "passwordLength" is between 8-128
 // If it not a valid number, alert the user it is not a valid number
@@ -34,6 +34,13 @@
 
 
 var passwordLength = "";
+var password = "";
+var passwordArray = "";
+var hasLowercase = "";
+var hasUpppercase = "";
+var hasNumbers = "";
+var hasSpecials = "";
+var random = "";
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppoercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
